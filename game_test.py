b2c60a53f07286bfd0cd42f516d6f679b536eb04
Raoul <nasj1621@gmail.com>
2023-03-27 19:09:38 +0900

    skill change ''

--- game_test.py
+++ game_test.py
@@ -2,23 +2,21 @@ import random
 import os
 import hello
 
-
+# 캐릭터 소환
 mon_max_hp = random.randrange(10, 100)
 mon_power = random.randrange(10, 50)
 pla_max_hp = random.randrange(50, 100)
 pla_power = random.randrange(10, 30)
 pla_max_mp = 10
-
-
 player = hello.Player("Default", pla_max_hp, pla_power, pla_max_mp)
-player.name = str(input("플레이어의 이름을 지정해 주세요 : "))
-
-os.system('clear')
 monster = hello.Monster("Monster", mon_max_hp, mon_power)
+
+# 캐릭터 이름 정하기
+player.name = str(input("플레이어의 이름을 지정해 주세요 : "))
+os.system('clear')
 
 
 def game_start():
-    os.system('clear')
     print("")
     print("----------@@@입장@@@----------")
     player.show_status()
@@ -29,11 +27,13 @@ def game_start():
         print("----------전장----------")
 
         skill = input("스킬을 선택해주세요.(마법공격(m)/일반공격(d)) : ")
-        if skill == "m" or "마법공격":
-            player.attack_mp(monster)
-        else:
+        if (skill == "d") or (skill == "일반공격"):
             player.attack(monster)
 
+        else:
+            player.attack_mp(monster)
+            # print("햅삐햅삐~~~~~~~")
+            # print(skill)
         monster.attack(player)
         # os.system('clear')
         print("")
@@ -42,7 +42,7 @@ def game_start():
         monster.show_status()
 
         if (player.hp <= 0) or (monster.hp <= 0):
-            flag = 0
+            flag = 0  # 확실히 꺼주려고
             break
 
     print("")
